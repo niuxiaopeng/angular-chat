720a33150512d04fda1c9a0e0fbf0ff9f289037b
niuxiaopeng <648279073@qq.com>
2020-03-11 19:26:45 +0900

    プロジェクトを新規します。最初版

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  // 取得したFirebaseの情報を設定してください
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  }
+};
