4952278dc9679e77ec3880f7a40e8006c67c70b2
git-user1 <648279073@qq.com>
2020-07-03 15:24:11 +0900

    本地调式程序
    本地调式程序所需配置

--- README.md
+++ README.md
@@ -25,3 +25,11 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## 这里需要注意的点
+
+如果在本地调式程序的时候，需要配置好自己本地的配置，因为这个项目目前目前部署在Firebase上，包括数据库也在FireBase上，所以对于Firebase的配置，至关重要。
+
+路径：C:\Users\xiaopeng.niu\Desktop\angular-lianxi\angular-chat-2\angular-chat\src\environments\environment.ts
+
+参考链接：[将 Firebase 添加到您的 JavaScript 项目](https://firebase.google.com/docs/web/setup?hl=zh-cn)
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,11 +7,12 @@ export const environment = {
   production: false,
   // 取得したFirebaseの情報を設定してください
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: "AIzaSyBzhUeyR2yywWB8n_IhZhzvpeFycaYXHqc",
+    authDomain: "angular-chat-63aa8.firebaseapp.com",
+    databaseURL: "https://angular-chat-63aa8.firebaseio.com/",
+    projectId: "angular-chat-63aa8",
+    storageBucket: "angular-chat-63aa8.appspot.com",
+    messagingSenderId: "440440617948",
+    appID: "1:440440617948:web:ea4b8b5775793432c6ed19"
   }
 };
